0009f712fb841b5344774584572501dd4657ee08
datavorous <datavorous.work@gmail.com>
2026-02-27 15:54:56 +0530

    feat: added file loading

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     int dim;
@@ -12,7 +13,7 @@ typedef struct {
 } Config;
 
 typedef struct {
-    int *token_embeddings;
+    int *token_embedding;
     float *rms_attention;
     float *wQ, *wK, *wV, *wO;
     float *w1, *w2, *w3;
@@ -35,7 +36,7 @@ typedef struct {
 typedef struct {
     Config config;
     Weights weights;
-    RunState run_state;
+    RunState state;
     float *data;
 } Transformer;
 
@@ -77,6 +78,105 @@ static void softmax(float *x, int n) {
     }
     for(int i = 0; i < n; i++)
         x[i] /= sum;
+}
+
+
+static void allocState(RunState *state, Config *config) {
+    int kv_dimensions = (config->dim * config->n_kv_heads) / config->num_heads;
+    state->x = malloc(config->dim * sizeof(float));
+    state->xb = malloc(config->dim * sizeof(float));
+    state->xb2 = malloc(config->dim * sizeof(float));
+
+    state->q = malloc(kv_dimensions * sizeof(float));
+    state->k = malloc(kv_dimensions * sizeof(float));
+    state->v = malloc(kv_dimensions * sizeof(float));
+
+    state->hb = malloc(config->hidden_dim * sizeof(float));
+    state->hb2 = malloc(config->hidden_dim * sizeof(float));
+
+    state->attention = malloc(config->seq_len * sizeof(float));
+    state->logits = malloc(config->vocab_size * sizeof(float));
+
+    state->key_cache = malloc(config->seq_len * kv_dimensions * sizeof(float));
+    state->value_cache = malloc(config->seq_len * kv_dimensions * sizeof(float));
+}
+
+static void wireWeights(Weights *weights, Config *config, float *pointer, int shared) {
+
+    int hs = config->dim / config->num_heads;
+    int kd = (config->dim * config->n_kv_heads) / config->num_heads;
+    long L = config->num_layers;
+
+    weights->token_embedding = pointer; 
+    pointer += config->vocab_size * config->dim;
+
+    weights->rms_attention = pointer;
+    pointer += L* config->dim;
+
+    weights->wQ = pointer;
+    pointer += L* config->dim * config->dim;
+    weights->wK = pointer;
+    pointer += L * config->dim * kd;
+    weights->wV = pointer;
+    pointer += L * config->dim * kd;
+    weights->wO = pointer;
+    pointer += L * config->dim * config->dim;
+
+    weights->rms_ffn = pointer;
+    pointer += L * config->dim;
+
+    weights->w1 = pointer;
+    pointer += L * config->dim * config->hidden_dim;
+    weights->w2 = pointer;
+    pointer += L * config->hidden_dim * config->dim;
+    weights->w3 = pointer;
+    pointer += L * config->dim * config->hidden_dim;
+
+    weights->rms_final = pointer;
+    pointer += config->dim;
+
+    pointer += config->seq_len * hs;
+    pointer += config->seq_len * hs;
+
+    weights->wcls = shared? weights->token_embedding : pointer;
+}
+
+static void loadTransformer(Transformer *transformer, const char *path) {
+    FILE *file = fopen(path, "rb");
+    fread(&transformer->config, sizeof(Config), 1, file);
+    int shared = transformer->config.vocab_size > 0;
+
+    transformer->config.vocab_size = abs(transformer->config.vocab_size);
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, sizeof(Config), SEEK_SET);
+    long wsize = file_size - sizeof(Config);
+    transformer->data = malloc(wsize);
+    fread(transformer->data, 1, wsize, file);
+    fclose(file);
+
+    wireWeights(&transformer->weights, &transformer->config, transformer->data, shared);
+    allocState(&transformer->state, &transformer->config);
+}
+
+static void loadTokenizer(Tokenizer *tokenizer, const char *path, int vocab_size) {
+    FILE *file = fopen(path, "rb");
+    fread(&tokenizer->max_token_len, sizeof(int), 1, file);
+
+    tokenizer->vocab_size = vocab_size;
+    tokenizer->vocab = malloc(vocab_size * sizeof(char*));
+    tokenizer->scores = malloc(vocab_size * sizeof(float));
+
+    for(int i = 0; i < vocab_size; i++) {
+        int len;
+        fread(&tokenizer->scores[i], sizeof(float), 1, file);
+        fread(&len, sizeof(int), 1, file);
+        tokenizer->vocab[i] = malloc(len + 1);
+        fread(tokenizer->vocab[i], 1, len, file);
+        tokenizer->vocab[i][len] = '\0';
+    }
+    fclose(file);
 }
 
 
