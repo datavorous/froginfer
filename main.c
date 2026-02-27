c889dd34681389fe8c2a4eb61bc274da3dafd56c
datavorous <datavorous.work@gmail.com>
2026-02-27 15:27:41 +0530

    feat: added math util functions

--- main.c
+++ main.c
@@ -47,6 +47,39 @@ typedef struct {
     unsigned char byte_pieces[512];
 } Tokenizer;
 
+
+static void matmul(float *out, float *x, float *W, int n, int d) {
+    for (int i = 0; i < d; i++) {
+        float val = 0;
+        for(int j =0; j < n; j++)
+            val += x[j] * W[j * d + i];
+        out[i] = val;
+    }
+}
+
+static void rmsnorm(float *out, float *x, float *w, int n) {
+    float ss = 0;
+    for(int i = 0; i < n; i++)
+        ss += x[i] * x[i];
+    ss = 1/sqrtf(ss/n + 1e-6);
+    for(int i = 0; i < n; i++)
+        out[i] = x[i] * ss * w[i];
+}
+
+static void softmax(float *x, int n) {
+    float max = x[0];
+    for(int i = 1; i < n; i++)
+        if(x[i] > max) max = x[i];
+    float sum = 0;
+    for(int i = 0; i < n; i++) {
+        x[i] = expf(x[i] - max);
+        sum += x[i];
+    }
+    for(int i = 0; i < n; i++)
+        x[i] /= sum;
+}
+
+
 int main() {
     return 0;
 }
